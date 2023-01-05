44a4428011781a7b1e54db462dca3381f7197fe1
Bing <hongbing0419@gmail.com>
2023-01-05 22:14:24 +0800

    ptr format

---
+++ ft_printptr.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printptr.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thong-bi <thong-bi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 17:18:52 by thong-bi          #+#    #+#             */
+/*   Updated: 2023/01/03 17:18:52 by thong-bi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "libft.h"
+
+int	ft_ptr_len(unsigned long num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		len++;
+		num /= 16;
+	}
+	return (len);
+}
+
+void	ft_put_ptr(unsigned long num)
+{
+	if(num >= 16)
+	{
+		ft_put_ptr(num / 16);
+		ft_put_ptr(num % 16);
+	}
+	else
+	{
+		if (num <= 9)
+			ft_printchar(num + '0');
+		else
+			ft_printchar(num - 10 + 'a');
+	}
+}
+
+int	ft_printptr(unsigned long ptr)
+{
+	int	len;
+
+	len = 0;
+	len += write(1, "0x", 2)
+	if (ptr == 0)
+		len += write(1, "0", 1);
+	else
+	{
+		ft_put_ptr(ptr);
+		len += ft_ptr_len(ptr);
+	}
+	return (len);
+}
